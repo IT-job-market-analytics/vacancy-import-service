5bc2f2dbec8ebb65998532ddd2ff4a3086c6b80f
Javohir <ismoilov92@gmail.com>
2023-06-13 21:46:57 +0300

    add project files

---
+++ src/main/java/com/example/vacancyimportservice/VacancyImportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacancyimportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyImportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyImportServiceApplication.class, args);
+	}
+
+}
