012468a8db57ce60efe6022213340e9192688c4a
Javohir <ismoilov92@gmail.com>
2023-06-28 23:43:32 +0300

    chore: add logger and exception handler for 400, 403 exceptions

--- src/main/java/com/example/vacancyimportservice/service/HHApiService.java
+++ src/main/java/com/example/vacancyimportservice/service/HHApiService.java
@@ -2,11 +2,13 @@ package com.example.vacancyimportservice.service;
 
 import com.example.vacancyimportservice.dto.VacancyImportScheduledTaskDto;
 import com.example.vacancyimportservice.dto.hh.Vacancies;
+import com.example.vacancyimportservice.exception.ApiRequestException;
 import com.example.vacancyimportservice.exception.ApiResponseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -23,16 +25,20 @@ public class HHApiService {
     }
 
     void query(VacancyImportScheduledTaskDto query) {
+        log.info("Receive schedule query: " + query);
         try {
             Vacancies vacancies = requestToApi(query);
+            log.info("Returned vacancies count:  " + vacancies.getItems().size());
             vacancies.getItems().forEach(item -> {
                 item.setQuery(query.getQuery());
                 produceService.publishVacancy(item);
             });
 
         } catch (ApiResponseException e) {
-            //TODO Обработать
+            log.info(e.getMessage());
             throw e;
+        } catch (ApiRequestException e) {
+            log.error(e.getMessage());
         }
     }
 
@@ -44,12 +50,23 @@ public class HHApiService {
                 .queryParam("page", query.getPageNumber())
                 .queryParam("per_page", query.getPageSize());
 
-        ResponseEntity<Vacancies> response = restTemplate.getForEntity(builder.toUriString(), Vacancies.class);
+        log.info("Request to HH API");
+        try {
+            ResponseEntity<Vacancies> response = restTemplate.getForEntity(builder.toUriString(), Vacancies.class);
 
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return response.getBody();
-        } else {
-            throw new ApiResponseException("Ошибка при выполнении запроса: " + response.getStatusCode());
+            if (response.getStatusCode().is2xxSuccessful()) {
+                log.info("Request completed successfully");
+                return response.getBody();
+            } else {
+                log.info("Request failed. Response code: " + response.getStatusCode());
+                throw new ApiResponseException("Ошибка при выполнении запроса: " + response.getStatusCode());
+            }
+        } catch (HttpClientErrorException.BadRequest e) {
+            log.info("Request failed. Bad request!");
+            throw new ApiRequestException(e.getMessage());
+        } catch (HttpClientErrorException.Forbidden e) {
+            log.info("Request failed. Too many request request!");
+            throw new ApiRequestException(e.getMessage());
         }
     }
 
