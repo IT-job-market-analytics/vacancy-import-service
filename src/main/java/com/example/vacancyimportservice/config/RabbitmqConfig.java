c51a76d9003585766b9a825086808027dd032f9b
Javohir <ismoilov92@gmail.com>
2023-06-29 21:53:10 +0300

    fix: delete exchange for imported-vacancies-queue

--- src/main/java/com/example/vacancyimportservice/config/RabbitmqConfig.java
+++ src/main/java/com/example/vacancyimportservice/config/RabbitmqConfig.java
@@ -1,6 +1,7 @@
 package com.example.vacancyimportservice.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -14,9 +15,6 @@ public class RabbitmqConfig {
     @Value("${rabbitmq.vacancies_queue}")
     private String vacanciesQueue;
 
-    @Value("${rabbitmq.vacancies_imported_exchange}")
-    private String vacanciesImportedExchange;
-
     @Bean
     public MessageConverter jsonMessageConvertor() {
         return new Jackson2JsonMessageConverter();
@@ -27,19 +25,6 @@ public class RabbitmqConfig {
         return new Queue(vacanciesQueue);
     }
 
-    @Bean
-    public DirectExchange exchange() {
-        return new DirectExchange(vacanciesImportedExchange);
-    }
-
-    @Bean
-    public Binding binding() {
-        return BindingBuilder
-                .bind(queue())
-                .to(exchange())
-                .with("");
-
-    }
     @Bean
     public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
