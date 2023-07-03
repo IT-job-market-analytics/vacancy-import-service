04583709af4cd4ed96dd24785229375ad3f85a74
Javohir <ismoilov92@gmail.com>
2023-07-03 03:25:52 +0300

    chore: build-and-push.sh

---
+++ build-and-push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IMAGE_NAME="itjobmarketanalytics-vacancy-import-service"
+TAG="dev"
+DOCKERHUB_NAME="vacusertest"
+
+docker build -t "${IMAGE_NAME}" .
+
+docker tag "${IMAGE_NAME}:latest" "${DOCKERHUB_NAME}/${IMAGE_NAME}:${TAG}"
+
+docker push "${DOCKERHUB_NAME}/${IMAGE_NAME}:${TAG}"
